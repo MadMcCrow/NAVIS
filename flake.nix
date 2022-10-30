351de7fb40fc84f912973e7fe606561d844e129c
MadMcCrow <noe.perard@live.ru>
2022-10-30 19:15:31 +0100

    :snowflake::sparkles: First commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Navis is a game made with the Godot Engine";
+  inputs = {
+    # godot engine, master branch
+    godot-flake = { 
+      url = "github:MadMcCrow/Godot-Flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      };
+  };
+
+  outputs = { self, nixpkgs, godot-flake, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs   = import nixpkgs { inherit system; };
+    godot  = inputs.godot-flake.packages."${system}".godot;
+
+    in rec {
+      packages."${system}" =  {
+
+      navis-game = pkgs.stdenv.mkDerivation rec {
+          name = "navis-game";
+          buildInputs = [godot];
+          runtimeDependencies = with pkgs; [ vulkan-loader libpulseaudio ];
+          src = self;
+          buildPhase = ''
+            ${godot}/bin/godot.bin -e 
+          '';
+        };
+
+	      default = pkgs.linkFarmFromDrvs "navis" [ packages."${system}".navis-game godot];
+      };
+
+      devShells."${system}".default =
+        pkgs.mkShell {
+          packages = [ godot ];
+        };
+
+    };
+}
